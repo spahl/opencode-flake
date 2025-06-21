49e4a87f41ae37f3f3d26205aa7a577e73bf6977
Aodhan Hayter <aodhan.hayter@gmail.com>
2025-06-21 17:31:18 -0600

    fix broken version from test script

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
           # Import the OpenCode package definition
           opencode = import ./package.nix {
             inherit pkgs system;
-            version = "0.1.118";
+            version = "0.1.117";
           };
 
           # Script to verify opencode --version works
